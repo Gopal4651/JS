eec015ab4f3364895cd8f8f276259d19cd19ef3d
Gopal4651 <121903475+Gopal4651@users.noreply.github.com>
2025-09-08 11:16:15 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144533
+let email = "gopal@gmail.com"
+var accountPassword  = "12345"
+accountCity = "Indore"
+
+console.table([accountId, accountPassword, email, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) we are using nodejs not browser
+
+//number => 2 power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
